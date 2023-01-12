7c6ba7ad3939d28bf89a918c5cd9afda99caaf55
LeoAlmiron <almironleandro86@gmail.com>
2023-01-12 19:57:51 -0300

    Back-End
    Back-End proyecto Argentina programa

---
+++ src/main/java/com/Portfolio/AP/Repository/IPersonaRepository.java
@@ -0,0 +1,13 @@
+
+package com.Portfolio.AP.Repository;
+
+import com.Portfolio.AP.Entity.Persona;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Integer> {
+    public Optional<Persona> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+}
